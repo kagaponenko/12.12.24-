fde82bab56b869b170d72b40d185eae5c95667ba
opilane <opilane@HP-CZC0359NNB>
2024-12-12 13:36:13 +0200

    Add project files.

---
+++ 12.12.24/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _12._12._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data types!");
+
+            //täisnumber
+            int bigNumber = 1244444445;
+            Console.WriteLine(bigNumber);
+
+            //komaga number
+            double comaNumber = 12.5;
+            Console.WriteLine(comaNumber);
+
+            //tähemärk
+            string stringVar = "lvcol¤#";
+            Console.WriteLine(stringVar);
+
+            float floatVarible = 123.123F;
+
+        }
+    }
+}
